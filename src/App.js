d41f797f2c22c9462e3ad1996e020976e429cc07
Bushra AlQahtani <89680878+bushra-alqahtani@users.noreply.github.com>
2022-08-22 10:29:02 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { useState } from 'react';
+import Tabs from './TabsComponents/Tabs'
+
+function App() {
+
+  return (
+      <div className='App'>
+        <Tabs/>
+          
+      </div>
+  );
+}
+
+export default App;
